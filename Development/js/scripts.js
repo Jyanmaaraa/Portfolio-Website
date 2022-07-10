cfbd21b03b54182c49ea2cad78b20347d154bfc4
Jyanmaaraa <68990907+Jyanmaaraa@users.noreply.github.com>
2022-07-10 10:45:21 +0545

    Add files via upload

---
+++ Development/js/scripts.js
@@ -0,0 +1,3 @@
+/*displays full date and time */ 
+var d = new Date();/*creating new date oject*/
+document.getElementById("date").innerHTML = d;/*changes innerhtml element content that has ID "date" */
